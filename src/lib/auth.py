74e1b527979583f2397c8317b1944f4da5b3e2ee
Gonzalo Ayuso <gonzalo123@gmail.com>
2024-05-19 14:58:28 +0200

    wip

--- src/lib/auth.py
+++
@@ -1,9 +0,0 @@
-from flask import request, abort
-
-
-def validate_bearer_token(headers, bearer_token: str):
-    if 'Authorization' not in headers:
-        abort(401)
-    data = request.headers['Authorization']
-    if str.replace(str(data), 'Bearer ', '') != bearer_token:
-        abort(401)
